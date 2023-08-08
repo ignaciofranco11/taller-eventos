ba4c1af46d068d983611e3eb8e1d4e2ce072249f
ignaciofranco11 <141186437+ignaciofranco11@users.noreply.github.com>
2023-08-08 19:50:48 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ var div = document.getElementById("div");
 div.addEventListener("click", function () {
     alert("Hola! Soy el div.");
 });
+
+function saludar(event) {
+    alert("Hola!");
+    event.stopPropagation();
+}
